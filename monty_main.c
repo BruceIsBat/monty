558f4d172ac042a86d62aeceea741440324391d9
BruceIsBat <awoteduibrahim@gmail.com>
2023-08-27 08:54:00 -0700

    LIFO

--- monty_main.c
+++ monty_main.c
@@ -1,50 +1,50 @@
 #include "monty.h"
-
+/**
+ * main - main function
+ * Return: 0
+ */
 int main(void)
 {
-    stack_t *stack = NULL;
-    char line[100];
-    unsigned int line_number = 1;
+	stack_t *stack = NULL;
+	char line[100];
+	unsigned int line_number = 1;
 
-    while (fgets(line, sizeof(line), stdin) != NULL)
-    {
-        char *opcode = strtok(line, " \n");
+	while (fgets(line, sizeof(line), stdin) != NULL)
+	{
+		char *opcode = strtok(line, " \n");
 
-        if (opcode == NULL)
-        {
-            /* Handle empty lines */
-            line_number++;
-            continue;
-        }
+		if (opcode == NULL)
+		{
+			line_number++;
+			continue;
+		}
+		if (strcmp(opcode, "push") == 0)
+		{
+			push(&stack, line_number);
+		}
+		else if (strcmp(opcode, "pall") == 0)
+		{
+			pall(&stack, line_number);
+		}
+		else if (strcmp(opcode, "pint") == 0)
+		{
+			pint(&stack, line_number);
+		}
+		else
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+			exit(EXIT_FAILURE);
+		}
 
-        if (strcmp(opcode, "push") == 0)
-        {
-            push(&stack, line_number);
-        }
-        else if (strcmp(opcode, "pall") == 0)
-        {
-            pall(&stack, line_number);
-        }
-        else if (strcmp(opcode, "pint") == 0)
-        {
-            pint(&stack, line_number);
-        }
-        /* Add other opcodes here */
-        else
-        {
-            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-            exit(EXIT_FAILURE);
-        }
-        
-        line_number++;
-    }
+		line_number++;
+	}
+	while (stack != NULL)
+	{
+		stack_t *temp = stack;
+		stack = stack->next;
 
-    while (stack != NULL)
-    {
-        stack_t *temp = stack;
-        stack = stack->next;
-        free(temp);
-    }
+		free(temp);
+	}
 
-    return 0;
+	return (0);
 }
