7c8f0778aad60d56749eb963b7d94f442a450f3f
BruceIsBat <awoteduibrahim@gmail.com>
2023-08-27 08:38:58 -0700

    LIFO

---
+++ monty_main.c
@@ -0,0 +1,50 @@
+#include "monty.h"
+
+int main(void)
+{
+    stack_t *stack = NULL;
+    char line[100];
+    unsigned int line_number = 1;
+
+    while (fgets(line, sizeof(line), stdin) != NULL)
+    {
+        char *opcode = strtok(line, " \n");
+
+        if (opcode == NULL)
+        {
+            /* Handle empty lines */
+            line_number++;
+            continue;
+        }
+
+        if (strcmp(opcode, "push") == 0)
+        {
+            push(&stack, line_number);
+        }
+        else if (strcmp(opcode, "pall") == 0)
+        {
+            pall(&stack, line_number);
+        }
+        else if (strcmp(opcode, "pint") == 0)
+        {
+            pint(&stack, line_number);
+        }
+        /* Add other opcodes here */
+        else
+        {
+            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+            exit(EXIT_FAILURE);
+        }
+        
+        line_number++;
+    }
+
+    while (stack != NULL)
+    {
+        stack_t *temp = stack;
+        stack = stack->next;
+        free(temp);
+    }
+
+    return 0;
+}
